f72d529b4d030e45ec7d621f95a7fab4d796fd2d
Pradyumna Shome <pradyumna.shome@gmail.com>
2019-06-06 21:49:41 -0700

    Added intern social matcher.
    - Changed repository structure

---
+++ src/intern-social-matcher/intern_social_matcher.py
@@ -0,0 +1,33 @@
+import itertools
+import random
+import sys
+import os
+
+
+def get_intern_pairings(intern_list, group_size):
+    random.shuffle(intern_list)
+
+    intern_count = len(intern_list)
+    return [
+        intern_list[x:x + group_size]
+        for x in range(0, intern_count, group_size)
+    ]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(
+            "See usage in README. Must provide path to file with a newline-delimited list of interns, and group size.",
+            file=sys.stderr)
+        sys.exit(os.EX_USAGE)
+
+    intern_list = None
+    with open(sys.argv[1]) as intern_list_file:
+        intern_list = intern_list_file.read().split()
+
+    group_size = int(sys.argv[2])
+
+    pairings = get_intern_pairings(intern_list=intern_list,
+                                   group_size=group_size)
+
+    print(pairings)
